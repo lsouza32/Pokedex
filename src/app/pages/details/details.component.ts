b0632f73e71f97bff50eafd645dafcc21695c4ca
Lucas Souza <lukssantos32@gmail.com>
2023-06-06 12:30:03 -0300

    Ajustado Details

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin, subscribeOn } from 'rxjs';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit{
 
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
+
+  public pokemon: any;
+
+  constructor(
+    private activatedRouter: ActivatedRoute,
+    private pokeApiService: PokeApiService,
+  ){}
+
+  ngOnInit(): void{
+    this.getPokemon;
+  }
+
+  get getPokemon(){
+
+    const id = this.activatedRouter.snapshot.params['id'];
+    const pokemon = this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
+    const name = this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
+
+    return forkJoin([pokemon, name]).subscribe(
+      res => {
+        this.pokemon = res;
+      }
+    );
+
+
+  }
 }
